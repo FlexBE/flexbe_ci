8d48aa155890fe48ceff5af6dc9f678308946194
Philipp Schillinger <philsplus@gmail.com>
2020-12-09 21:54:59 +0100

    Fix sourcing of devel tests

--- ci_scripts/run_devel_tests.bash
+++ ci_scripts/run_devel_tests.bash
@@ -6,6 +6,6 @@ cd ~/catkin_ws
 catkin clean --yes
 catkin config --no-install
 catkin build
-source ~/catkin_ws/install/setup.bash
+source ~/catkin_ws/devel/setup.bash
 xvfb-run rosrun flexbe_app run_app --offline --run-tests
 catkin run_tests && catkin_test_results
