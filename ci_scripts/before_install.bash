a553d38a8b53d5d67f8b8e781126ce11ba395a0d
Philipp Schillinger <philsplus@gmail.com>
2020-12-09 17:00:56 +0100

    Use python3 version of basic apt packages for ROS noetic

--- ci_scripts/before_install.bash
+++ ci_scripts/before_install.bash
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+PYTHON=${PYTHON:-python}
+
 # install ros
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
@@ -8,6 +10,6 @@ sudo apt-get update
 sudo apt-get install -y dpkg # fix https://github.com/travis-ci/travis-ci/issues/10132
 sudo apt install -y xvfb  # `xvfb-run` is used in scrpt.bash
 sudo apt-get install -y ros-$ROS_DISTRO-ros-base
-sudo apt-get install -y python-rosinstall python-rosinstall-generator python-wstool build-essential python-catkin-pkg python-rosdep ros-$ROS_DISTRO-catkin python-catkin-tools
+sudo apt-get install -y $PYTHON-rosinstall $PYTHON-rosinstall-generator $PYTHON-wstool build-essential $PYTHON-catkin-pkg $PYTHON-rosdep ros-$ROS_DISTRO-catkin $PYTHON-catkin-tools
 sudo rosdep init
 rosdep update
