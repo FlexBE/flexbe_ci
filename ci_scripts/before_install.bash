f75f3121d3b3d2c52a1434242c53f578c2b2a568
David Conner <robotics@cnu.edu>
2023-06-13 21:50:06 -0400

    update for ros2 install

--- ci_scripts/before_install.bash
+++ ci_scripts/before_install.bash
@@ -3,13 +3,17 @@ set -e
 
 PYTHON=${PYTHON:-python}
 
-# install ros
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-sudo apt-get update
-sudo apt-get install -y dpkg # fix https://github.com/travis-ci/travis-ci/issues/10132
+# install ros2
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt update
+sudo apt install -y dpkg # fix https://github.com/travis-ci/travis-ci/issues/10132
 sudo apt install -y xvfb  # `xvfb-run` is used in scrpt.bash
-sudo apt-get install -y ros-$ROS_DISTRO-ros-base
-sudo apt-get install -y $PYTHON-rosinstall $PYTHON-rosinstall-generator $PYTHON-wstool build-essential $PYTHON-catkin-pkg $PYTHON-rosdep ros-$ROS_DISTRO-catkin $PYTHON-catkin-tools $PYTHON-osrf-pycommon
+sudo apt install -y ros-$ROS_DISTRO-ros-base
+sudo apt install -y $PYTHON-rosinstall $PYTHON-rosinstall-generator $PYTHON-wstool ros-dev-tools $PYTHON-catkin-pkg $PYTHON-rosdep ros-$ROS_DISTRO-catkin $PYTHON-catkin-tools $PYTHON-osrf-pycommon
 sudo rosdep init
 rosdep update --rosdistro=${ROS_DISTRO}
