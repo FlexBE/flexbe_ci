b2ca85e72a2ea5ec5de09eb5267cdf90d37d4926
David Conner <robotics@cnu.edu>
2023-07-26 11:51:56 -0400

    add rosdep call after create repo

--- ci_scripts/test_create_repo.bash
+++ ci_scripts/test_create_repo.bash
@@ -7,5 +7,6 @@ source ~/colcon_ws/install/setup.bash
 cd ~/colcon_ws/src
 ros2 run flexbe_widget create_repo test --non-interactive
 cd ~/colcon_ws
+rosdep install -y --from-paths src --ignore-src --rosdistro=${ROS_DISTRO}
 colcon build
 source ~/colcon_ws/install/setup.bash
