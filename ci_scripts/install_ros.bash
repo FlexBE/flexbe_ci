cf50560d42650056726753d50378b166b433f974
David Conner <robotics@cnu.edu>
2024-06-04 11:56:33 -0400

    attempting cleanup for Jazzy

--- ci_scripts/install_ros.bash
+++ ci_scripts/install_ros.bash
@@ -4,13 +4,15 @@ set -e
 PYTHON=${PYTHON:-python}
 
 # install ros 2
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+sudo apt update
+sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 sudo apt update
-sudo apt install -y dpkg # fix https://github.com/travis-ci/travis-ci/issues/10132
-sudo apt install -y xvfb  # `xvfb-run` is used in scrpt.bash
+sudo apt install ros-dev-tools
 sudo apt install -y ros-$ROS_DISTRO-ros-base
-sudo apt install -y $PYTHON-rosinstall $PYTHON-rosinstall-generator $PYTHON-wstool ros-dev-tools $PYTHON-catkin-pkg $PYTHON-rosdep $PYTHON-osrf-pycommon
 sudo rosdep init
 rosdep update --rosdistro=${ROS_DISTRO}
