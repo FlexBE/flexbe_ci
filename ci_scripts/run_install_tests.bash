9f5aa82e242f49d73a565a1e488e60b812b55816
David Conner <robotics@cnu.edu>
2024-06-04 11:11:01 -0400

    switch to Jazzy and do not test flexbe_app here

--- ci_scripts/run_install_tests.bash
+++ ci_scripts/run_install_tests.bash
@@ -7,7 +7,8 @@ cd ~/colcon_ws
 rm -rf install build log
 colcon build
 source ~/colcon_ws/install/setup.bash
-ros2 run flexbe_app nwjs_install
-xvfb-run ros2 run flexbe_app run_app --offline --run-tests
+# -- No longer testing flexbe_app here given release of flexbe_webui
+# ros2 run flexbe_app nwjs_install
+# xvfb-run ros2 run flexbe_app run_app --offline --run-tests
 colcon test --ctest-args tests
 colcon test-result --all
