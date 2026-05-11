a12553081b9ce6884cfb0c3cb5bd1e56c01cf829
David Conner <robotics@cnu.edu>
2026-05-11 14:02:20 -0400

    only use create_repo if dependencies are statisfied (e.g. for new rolling)

--- ci_scripts/test_create_repo.bash
+++ ci_scripts/test_create_repo.bash
@@ -7,6 +7,12 @@ source ~/colcon_ws/install/setup.bash
 cd ~/colcon_ws/src
 ros2 run flexbe_widget create_repo test --non-interactive
 cd ~/colcon_ws
-rosdep install -y --from-paths src --ignore-src --rosdistro=${ROS_DISTRO}
+
+if ! rosdep install -y --from-paths src --ignore-src --rosdistro=${ROS_DISTRO}; then
+    echo "WARNING: rosdep install failed for test repo - skipping build, removing test repo"
+    rm -rf ~/colcon_ws/src/test
+    exit 0
+fi
+
 colcon build
 source ~/colcon_ws/install/setup.bash
